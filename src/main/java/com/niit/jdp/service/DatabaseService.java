242b0ef6e053b91706d07c38ec5f1e8c61f21d9e
Anusha <anusundar98@gmail.com>
2022-11-30 18:01:22 +0530

    added connect() to DatabaseService.java

--- src/main/java/com/niit/jdp/service/DatabaseService.java
+++ src/main/java/com/niit/jdp/service/DatabaseService.java
@@ -8,10 +8,16 @@
 package com.niit.jdp.service;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseService {
     private static final String URL = "jdbc:mysql://localhost:3306/songs";
     private static final String userName = "root";
     private static final String password = "root";
     Connection connection;
+
+    private void connect() throws SQLException {
+        connection = DriverManager.getConnection(URL, userName, password);
+    }
 }
