91a5bd40c6a81a186174421003c66ac87261147f
Anusha <anusundar98@gmail.com>
2022-11-30 17:59:43 +0530

    imported Connection interface

--- src/main/java/com/niit/jdp/service/DatabaseService.java
+++ src/main/java/com/niit/jdp/service/DatabaseService.java
@@ -7,8 +7,11 @@
 
 package com.niit.jdp.service;
 
+import java.sql.Connection;
+
 public class DatabaseService {
     private static final String URL = "jdbc:mysql://localhost:3306/songs";
     private static final String userName = "root";
     private static final String password = "root";
+    Connection connection;
 }
