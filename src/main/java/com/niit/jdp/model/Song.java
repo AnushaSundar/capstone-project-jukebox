9a189807b02870a1781f8307198a4c4707d8c005
Anusha <anusundar98@gmail.com>
2022-11-30 18:07:25 +0530

    added getter and setter methods to all field in Song.java

--- src/main/java/com/niit/jdp/model/Song.java
+++ src/main/java/com/niit/jdp/model/Song.java
@@ -26,4 +26,52 @@ public class Song {
         this.language = language;
         this.songDuration = songDuration;
     }
+
+    public int getSongId() {
+        return songId;
+    }
+
+    public void setSongId(int songId) {
+        this.songId = songId;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public void setSongName(String songName) {
+        this.songName = songName;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public double getSongDuration() {
+        return songDuration;
+    }
+
+    public void setSongDuration(double songDuration) {
+        this.songDuration = songDuration;
+    }
 }
