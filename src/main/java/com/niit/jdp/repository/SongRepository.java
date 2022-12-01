707baa431e74ae4d893d657cafaaa991ea0d5749
Anusha <anusundar98@gmail.com>
2022-12-01 12:20:53 +0530

    defined searchSongByArtistLanguageGenre() in SongRepository.java

--- src/main/java/com/niit/jdp/repository/SongRepository.java
+++ src/main/java/com/niit/jdp/repository/SongRepository.java
@@ -11,10 +11,7 @@ import com.niit.jdp.model.Song;
 import com.niit.jdp.service.DatabaseService;
 import com.niit.jdp.service.MusicPlayerService;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,8 +28,7 @@ public class SongRepository {
     public List<Song> displaySongs() {
         songList = new ArrayList<>();
         String selectQuery = "select * from `songs`.`song`;";
-        try {
-            Statement statement = connection.createStatement();
+        try (Statement statement = connection.createStatement()) {
             ResultSet resultSet = statement.executeQuery(selectQuery);
             while (resultSet.next()) {
                 int songId = resultSet.getInt("song_id");
@@ -57,8 +53,31 @@ public class SongRepository {
         for (Song song : songList) {
             musicPlayerService.play(song.getSongPath());
         }
+        System.out.println("Song is playing");
     }
 
-    public void searchSongByArtistLanguageGenre() {
+    public List<Song> searchSongByArtistLanguageGenre(String columnName, String value) {
+        List<Song> songSortedList = new ArrayList<>();
+        String selectQuery = "select * from `songs`.`song` where (?= ?);";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(selectQuery)) {
+            preparedStatement.setString(1, columnName);
+            preparedStatement.setString(2, value);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                int songId = resultSet.getInt("song_id");
+                String songName = resultSet.getString("song_name");
+                String genre = resultSet.getString("genre");
+                String artist = resultSet.getString("artist");
+                double songDuration = resultSet.getDouble("song_duration");
+                String language = resultSet.getString("language");
+                String album = resultSet.getString("album");
+                String songPath = resultSet.getString("song_path");
+                Song song = new Song(songId, songName, genre, artist, language, songDuration, songPath);
+                songSortedList.add(song);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return songSortedList;
     }
 }
