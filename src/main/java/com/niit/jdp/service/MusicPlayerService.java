d090fd9edc8d38e2fc2160563a72c39427bbe161
Anusha <anusundar98@gmail.com>
2022-12-01 09:51:56 +0530

    added play() to MusicPlayerService.java

--- src/main/java/com/niit/jdp/service/MusicPlayerService.java
+++ src/main/java/com/niit/jdp/service/MusicPlayerService.java
@@ -7,5 +7,22 @@
 
 package com.niit.jdp.service;
 
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
 public class MusicPlayerService {
+    public void play(String path) {
+        File file = new File(path);
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+            Thread.sleep(clip.getMicrosecondLength() / 1000L);
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException |
+                 InterruptedException exception) {
+            exception.printStackTrace();
+        }
+    }
 }
