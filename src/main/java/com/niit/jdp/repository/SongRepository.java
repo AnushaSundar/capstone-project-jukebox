d20f87624aead6458eac6f5beb7332549885144b
Anusha <anusundar98@gmail.com>
2022-12-01 11:53:12 +0530

    added no-args constructor to SongRepository.java and established connection

--- src/main/java/com/niit/jdp/repository/SongRepository.java
+++ src/main/java/com/niit/jdp/repository/SongRepository.java
@@ -7,8 +7,24 @@
 
 package com.niit.jdp.repository;
 
+import com.niit.jdp.model.Song;
+import com.niit.jdp.service.DatabaseService;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
 public class SongRepository {
-    public void displaySongs() {
+    DatabaseService databaseService;
+    Connection connection;
+
+    public SongRepository() throws SQLException {
+        databaseService = new DatabaseService();
+        connection = databaseService.getConnectionToDatabase();
+    }
+
+    public List<Song> displaySongs() {
+
     }
 
     public void playSong() {
--- src/main/java/com/niit/jdp/service/MusicPlayerService.java
+++ src/main/java/com/niit/jdp/service/MusicPlayerService.java
@@ -34,4 +34,5 @@ public class MusicPlayerService {
         clip.stop();
         clip.close();
     }
+
 }
