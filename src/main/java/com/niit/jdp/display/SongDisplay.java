07dcf19b13236652e23443b3719ac0fc6f15d8b8
Anusha <anusundar98@gmail.com>
2022-12-02 15:17:23 +0530

    added jukeboxDisplay() in SongDisplay.java

--- src/main/java/com/niit/jdp/display/SongDisplay.java
+++ src/main/java/com/niit/jdp/display/SongDisplay.java
@@ -8,4 +8,12 @@
 package com.niit.jdp.display;
 
 public class SongDisplay {
+    public void jukeboxDisplay() {
+        System.out.println("===================================");
+        System.out.println("       Welcome To Jukebox          ");
+        System.out.println("===================================");
+        System.out.println();
+        System.out.println("1.Go to Songs");
+        System.out.println("2.Exit");
+    }
 }
