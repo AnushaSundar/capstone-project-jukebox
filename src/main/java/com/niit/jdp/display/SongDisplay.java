91926e66c6f610f75beca57e233be86fd2c8d4b4
Anusha <anusundar98@gmail.com>
2022-12-03 13:46:53 +0530

    added createPlaylist() in SongDisplay.java

--- src/main/java/com/niit/jdp/display/SongDisplay.java
+++ src/main/java/com/niit/jdp/display/SongDisplay.java
@@ -244,4 +244,23 @@ public class SongDisplay {
             }
         } while (option6 != 3);
     }
+
+    public void createPlaylist() {
+        System.out.println("Enter the playlist name: ");
+        String name = scanner.next();
+        Playlist playlist1 = playlistRepository.createPlaylist(name);
+        System.out.println("successfully created your playlist.");
+        System.out.println("Your playlist id is: " + playlist1.getPlaylistId());
+        System.out.println("Want to see the playlist then press 1 or 0 to exit");
+        int option7 = scanner.nextInt();
+        List<Playlist> playlist2 = null;
+        if (option7 == 1) {
+            playlist2 = playlistRepository.getPlaylist();
+            playlistRepository.displayPlaylist(playlist2);
+            System.out.println();
+            break;
+        }
+        if (option7 == 0)
+            break;
+    }
 }
